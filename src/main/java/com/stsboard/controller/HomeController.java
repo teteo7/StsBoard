80c1765f0b0f750c879ec9a86537cc188dd1ae1e
teteo7 <teteocomputer@gmail.com>
2021-12-24 03:12:31 +0900

    이미지 폴더에 저장

--- src/main/java/com/stsboard/controller/HomeController.java
+++ src/main/java/com/stsboard/controller/HomeController.java
@@ -1,12 +1,12 @@
 package com.stsboard.controller;
 
-import java.util.Map;
+import java.io.File;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 
 
@@ -17,6 +17,31 @@ public class HomeController {
 	public String home() {
 		
 		return "home";
+	}
+	
+	@PostMapping("/upload_image")
+	public void update_action(MultipartFile[] upload_file, Model model) {
+		
+		
+		String uploadFolder = "C:\\upload\\temp";
+		System.out.println("여기는 업로드 이미지 에요.");
+		for(MultipartFile multipartFile : upload_file) {
+			System.out.println("포문 안쪽.");
+			
+			System.out.println("======================");
+			System.out.println("업로드된 파일 이름: " + multipartFile.getOriginalFilename());
+			System.out.println("업로드된 파일 크기: " + multipartFile.getSize());
+			
+			
+			File saveFile = new File(uploadFolder,multipartFile.getOriginalFilename());
+			
+			try {
+				multipartFile.transferTo(saveFile);
+			}catch(Exception e) {
+				System.out.println("파일저장 실패");
+			}
+		}
+		return;
 	}
 	
 }
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,27 +1,26 @@
-<%@ page language= "java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<c:set var="path" value="${pageContext.request.contextPath}"/>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<c:set var="path" value="${pageContext.request.contextPath}" />
 
 <html>
 <head>
-	<title>Home</title>
+<title>Home</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
+	<h1>Hello world!</h1>
 
-<P>  The time on the server is ${serverTime}. </P>
-<p>여기는 홈이에요</p>
+	<P>The time on the server is ${serverTime}.</P>
+	<p>여기는 홈이에요</p>
 
+	<form action="/upload_image" id="image_upload_form" method="post" enctype="multipart/form-data">
+		<input type="file" name="uploadFile" multiple>
+		<input type="submit">
+	</form>
 
-<!--Button을 누르면 idCheck로 요청  -->
 
 	<a href="/JqueryAjax">JqueryAjax</a>
-	
-<script type="text/javascript" src="${path}\resources\js\jquery-3.6.0.min.js"></script>
-
- 
+<%-- 	<script type="text/javascript"	src="${path}\resources\js\jquery-3.6.0.min.js"></script> --%>
 
 </body>
 </html>
